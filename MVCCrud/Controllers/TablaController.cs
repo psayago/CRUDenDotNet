54bcd286334542138972843965f4c59f53109120
Pablo Sayago <psayago@est.frba.utn.edu.ar>
2020-06-19 02:35:43 -0300

    first commit

---
+++ MVCCrud/Controllers/TablaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCrud.Models;
+using MVCCrud.Models.viewModels;
+
+namespace MVCCrud.Controllers
+{
+    public class TablaController : Controller
+    {
+        // GET: Tabla
+        public ActionResult Index()
+        {
+            List<ListTablaViewModel> lst;
+            using (SampleEntityFrameworkEntities db = new SampleEntityFrameworkEntities())
+            {
+                lst = (from d in db.tabla
+                       select new ListTablaViewModel()
+                       {
+                           Id = d.id,
+                           Nombre = d.nombre,
+                           Correo = d.correo,
+                           Fecha_nacimiento = d.fecha_nacimiento
+                       }
+                       ).ToList();
+
+            }
+
+            return View(lst);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (SampleEntityFrameworkEntities db = new SampleEntityFrameworkEntities())
+                    {
+                        var oTabla = new tabla();
+                        oTabla.nombre = model.Nombre;
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        db.tabla.Add(oTabla);
+                        db.SaveChanges();
+                    }
+                    return Redirect("/Tabla");
+                }
+                return View(model);
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        public ActionResult Editar(int id)
+        {
+            TablaViewModel model = new TablaViewModel();
+            using(SampleEntityFrameworkEntities db= new SampleEntityFrameworkEntities())
+            {
+                var oTabla = db.tabla.Find(id);
+                model.Nombre = oTabla.nombre;
+                model.Correo = oTabla.correo;
+                model.Fecha_Nacimiento = oTabla.fecha_nacimiento;
+                model.Id = oTabla.id;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaViewModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (SampleEntityFrameworkEntities db = new SampleEntityFrameworkEntities())
+                    {
+                        var oTabla = db.tabla.Find(model.Id);
+                        oTabla.nombre = model.Nombre;
+                        oTabla.correo = model.Correo;
+                        oTabla.fecha_nacimiento = model.Fecha_Nacimiento;
+                        db.Entry(oTabla).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                    return Redirect("/Tabla");
+                }
+                return View(model);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+        }
+
+        
+        
+        public ActionResult Eliminar(int id)
+        {
+            
+            using (SampleEntityFrameworkEntities db = new SampleEntityFrameworkEntities())
+            {
+                var oTabla = db.tabla.Find(id);
+                if (oTabla != null)
+                {
+                    db.tabla.Remove(oTabla);
+                    db.SaveChanges();
+                }
+                
+
+            }
+            return Redirect("/Tabla");
+
+        }
+    }
+}
---
+++ MVCCrud/Models/viewModels/TablaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Web;
+
+namespace MVCCrud.Models.viewModels
+{
+    public class TablaViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name ="Nombre")]
+        public string Nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        [Display(Name = "Correo electrónico")]
+        public string Correo { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha_Nacimiento { get; set; }
+    }
+}
